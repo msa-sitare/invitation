f18dca5d338b518d045877797dd45819d052e55e
msa-sitare <atessude@hotmail.com>
2024-04-24 15:52:02 +0200

    complete walk-scratch cat2

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   const cat1 = document.getElementById("cat1");
+  const cat2 = document.getElementById("cat2");
 
-  cat1.addEventListener("animationend", function(e) {
+  cat1.addEventListener("animationend", function (e) {
     if (e.animationName === "walkCycle") {
-      cat1.style.animation = 'scratchCycle 3s steps(8) 1 forwards';
+      cat1.style.animation = "scratchCycle 3s steps(8) 1 forwards";
+    }
+  });
+
+  cat2.addEventListener("animationend", function (e) {
+    if (e.animationName === "walkCycle") {
+      cat2.style.animation = "scratchCycle2 4s steps(12) 1 forwards";
     }
   });
 });
