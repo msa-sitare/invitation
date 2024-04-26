0f474b71ff6432b806b1b91b5db62c81c0ee0208
msa-sitare <atessude@hotmail.com>
2024-04-26 18:38:50 +0200

    complete all test1

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-/*document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const cat1 = document.getElementById("cat1");
   const cat2 = document.getElementById("cat2");
   const curtain1 = document.querySelector(".curtain1");
   const curtain2 = document.querySelector(".curtain2");
   const curtain3 = document.querySelector(".curtain3");
+  const sign1 = document.querySelector(".sign1");
+  const sign2 = document.querySelector(".sign2");
+  const sign3 = document.querySelector(".sign3");
+  const sign5 = document.querySelector(".sign5");
 
   cat1.addEventListener("animationend", function (e) {
     if (e.animationName === "walkCycle") {
@@ -17,6 +21,20 @@
 
           setTimeout(function () {
             curtain3.style.display = "none";
+
+            setTimeout(function () {
+              sign1.style.display = "none";
+
+              setTimeout(function () {
+                sign2.style.display = "none";
+
+                setTimeout(function () {
+                  sign3.style.display = "none";
+                  
+                  sign5.style.animation = "walkIn3 9s linear infinite";
+                }, 700);
+              }, 700);
+            }, 700);
           }, 700);
         }, 700);
       }, 3000);
@@ -28,4 +46,4 @@
       cat2.style.animation = "scratchCycle2 4s steps(12) 1 forwards";
     }
   });
-}); */
+});
